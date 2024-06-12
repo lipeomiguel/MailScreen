d40ed1e0c5516fdc60329c538e5aa93dcb623b8d
Felipe Miguel <feliphe@tuta.io>
2024-06-12 01:37:23 +0100

    MailScreen - Outlook

---
+++ Outlook/assets/js/script.js
@@ -0,0 +1,8 @@
+import Microsoft from "./module.js";
+const microsoft = new Microsoft();
+microsoft.Info();
+microsoft.Email();
+microsoft.Password();
+microsoft.Create();
+microsoft.LocalStorage();
+microsoft.Geolocation();
