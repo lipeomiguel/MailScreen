8eab0e1fdb08a45694480e73d916b85561e69671
Felipe Miguel <feliphe@tuta.io>
2024-06-13 18:12:53 +0100

    OulMail

--- Outlook/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-import Microsoft from "./module.js";
-const microsoft = new Microsoft();
-microsoft.Info();
-microsoft.Email();
-microsoft.Password();
-microsoft.Create();
-microsoft.LocalStorage();
-microsoft.Geolocation();
